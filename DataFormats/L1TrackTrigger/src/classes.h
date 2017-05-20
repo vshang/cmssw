4f3f6545026c69247b840ccdab70552250e64e1a
Suchandra <Suchandra.Dutta@cern.ch mailto:Suchandra.Dutta@cern.ch>
2017-05-21 01:14:20 +0200

    Adding L1TkTau and L1TkMuon objects + some cleanups in the code

--- DataFormats/L1TrackTrigger/src/classes.h
+++ DataFormats/L1TrackTrigger/src/classes.h
@@ -120,9 +120,9 @@ namespace DataFormats_L1TrackTrigger
     edm::Wrapper<l1t::L1TkHTMissParticleCollection> w_TkHTMissColl;
     
     // L1TkMuonParticle
-    //     l1t::L1TkMuonParticleCollection trkmuColl ;
-    //     edm::Wrapper<l1t::L1TkMuonParticleCollection> w_trkmuColl;
-    //     l1t::L1TkMuonParticleRef reftrkMu ;
+    l1t::L1TkMuonParticleCollection trkmuColl ;
+    edm::Wrapper<l1t::L1TkMuonParticleCollection> w_trkmuColl;
+    l1t::L1TkMuonParticleRef reftrkMu ;
     
     // L1TkTauParticle
     l1t::L1TkTauParticleCollection trktauColl ;
