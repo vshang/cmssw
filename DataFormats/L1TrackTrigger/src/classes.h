9f97b60a80c4cbd1f2e3e28001b055e940eaecc1
Marina Toumazou <mtoumazo@cern.ch>
2019-02-15 21:04:00 +0100

    [Phase2L1Taus]: Added EDProducers, EDAnalyzers and DataFormats for L1TkEGTaus and L1TkTaus - still in development.

--- DataFormats/L1TrackTrigger/src/classes.h
+++ DataFormats/L1TrackTrigger/src/classes.h
@@ -101,6 +101,22 @@ namespace
     //l1t::L1TkEmParticleRefVector refVectrkEmColl ;
     //l1t::L1TkEmParticleVectorRef vecReftrkEmColl ;
     //edm::reftobase::Holder<reco::Candidate, l1t::L1TkEmParticleRef> rtbtrke;
+
+    // L1TkEGTauParticle
+    l1t::L1TkEGTauParticleCollection trkegColl ;
+    edm::Wrapper<l1t::L1TkEGTauParticleCollection> w_trkegColl;
+    l1t::L1TkEGTauParticleRef reftrkEG ;
+    //l1t::L1TkEGTauParticleRefVector refVectrkEGColl ;
+    //l1t::L1TkEGTauParticleVectorRef vecReftrkEGColl ;
+    //edm::reftobase::Holder<reco::Candidate, l1t::L1TkEGTauParticleRef> rtbtrke;
+
+    // L1TrkTauParticle
+    l1t::L1TrkTauParticleCollection tktauColl ;
+    edm::Wrapper<l1t::L1TrkTauParticleCollection> w_tktauColl;
+    l1t::L1TrkTauParticleRef reftkTau ;
+    //l1t::L1TrkTauParticleRefVector refVectrkEGColl ;
+    //l1t::L1TrkTauParticleVectorRef vecReftrkEGColl ;
+    //edm::reftobase::Holder<reco::Candidate, l1t::L1TrkTauParticleRef> rtbtrke;
     
     // L1TkElectronParticle
     l1t::L1TkElectronParticleCollection trkeleColl ;
